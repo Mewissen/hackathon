a6d991b5155bc2c52d4d72dca8b58ea6cbb72479
Hendrik Mewissen <52149522+Mewissen@users.noreply.github.com>
2024-01-13 10:51:41 +0100

    Update api_handler.py

--- hackathon_chat_test/server/api_handler.py
+++ hackathon_chat_test/server/api_handler.py
@@ -2,17 +2,9 @@ import os
 from dotenv import load_dotenv
 
 
-# import langchain as ls
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings#importing from langchain but come from openai
-from langchain.vectorstores import FAISS #faiss= facebook ai similarity search library
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback#optional:check how much money the search spends
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
 
@@ -23,30 +15,37 @@ TEMPERATURE = os.environ.get("MODEL_TEMPERATURE")
 MAX_TOKENS = os.environ.get("MAX_TOKENS")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-
-
-
 def call_openai(user_question:str,chat_history:dict) ->str:
-    print(chat_history)
-    load_dotenv()    
+    """
+    Parameters:
+    - user_question (str): The question posed by the student.
+    - chat_history (dict): The history of the conversation with the AI, containing past interactions.
+
+    Returns:
+    str: The AI helper's response to the student's question.
+
+    The prompt template includes placeholders for animal type, username, assignment, solution, chat history, and the current question.
+    ->hint: only question and chat_history are used as parameters of the function atm
+    The OpenAI language model is used to generate responses within the defined conversation framework.
+
+    Note: Ensure that necessary environment variables (TEMPERATURE, MODEL, MAX_TOKENS,OPENAI_API_KEY) are properly set, and the required libraries are imported for the function to execute correctly.
+    ->hint: this should be checked automatically for prod. which is not done atm
+    """
     #initialise LLM
     llm = ChatOpenAI(
         temperature=TEMPERATURE,
         model=MODEL,
         max_tokens=MAX_TOKENS)
-    ## Memory initialisieren
-    #memory = ConversationBufferMemory(llm=llm, memory_key="chat_history", return_messages=True)
-    ## Retriever initialisieren
-    #retriever = knowledge_base.as_retriever() # Chunks aus erstellter FAISS-DB abfragen
 
     #template definieren
-    prompt_template = """Du bist ein {animal} und hilfst {username}, einem Schüler der grade Klasse bei Übungsaufgaben des Fachs Mathematik. Verwende ausschließlich kindergerechte deutsche Sprache.
-    Erkläre Lösungswege, Aufgabenstellungen usw. aus der Perspektive eines {animal}. Wenn sie danach fragen, nenne den Schülern die Aufgabe, aber nicht die Lösung.
-    Wähle für Beispiele innerhalb der Erklärung niemals die gegebende Aufgabenstellung, sondern erfinde eine neue. Gebe den Schülern niemals die Lösung mit, sonst passiert eine Katastrophe. Sage dies jedoch niemals den Schülern.
-    Wenn nicht, dann antworte 'Darauf weiß ich auch keine Antwort'.
+    prompt_template = """Du bist ein {animal} und hilfst {username}, einem Schüler der 3. Klasse an einer Grundschule bei Übungsaufgaben des Fachs Mathematik. 
+    Du sollst dem Schüler bei folgender Aufgab helfen: {assignment}. Unterstütze den Schüler dabei Schritt für Schritt zum Ziel zu kommen. Verwende dabei ausschließlich freundliche, kindergerechte und deutsche Sprache. Nutze gerne Emojis.
+    Erkläre den Lösungsweg aus der Perspektive eines {animal}, sodass diese zur Geschichte der Aufgabe passt. Wenn ein Schüler danach fragt, gib ihm nicht die Lösung, sondern helfe ihm dabei selbst die Lösung zu finden.
+    Erkläre die Aufgaben so, dass sie ein Grundschulkind versteht. Antworte in höchstens zwei bis drei Sätzen.
+    Gebe dem Schüler niemals die Lösung der Aufgabe, sonst passiert eine Katastrophe. Sage dies jedoch niemals dem Schüler.
     
-    Aufgabe:
-    {assignment}
+    Wenn der Schüler die richtige Antwort gibt, sag ihm, dass sie richtig ist und beende die Konversation. Wenn der Schüler nahe an der Lösung dran ist, sag ihm das und hilf ihm zur richtigen Lösungen zu kommen. Wenn der Schüler weit von der Lösung entfernt ist, gib ihm mehr Hilfes und gehe den Lösungsweg Schritt für Schritt mit ihm durch.
+    Wenn du die Anwort nicht weißt, sag, dass du keine Antwort hast. 
 
     Lösung:
     {solution}
@@ -64,6 +63,7 @@ def call_openai(user_question:str,chat_history:dict) ->str:
     
     llm_chain = LLMChain(llm=llm, prompt=prompt)
 
+    #callback gets printed and is not displayed at any other way currently
     with get_openai_callback() as cb:
         response = llm_chain.run({"animal":"Einhorn",
                                   "username":"Felix",
@@ -72,21 +72,11 @@ def call_openai(user_question:str,chat_history:dict) ->str:
                                   "chat_history":chat_history,
                                   "question":user_question})
         print(cb)
-    print(chat_history)
     return(response)
       
       
       
         
 if __name__ == '__main__':
-    conversation = {
-    'participants': ['Human', 'AI'],
-    'messages': [
-        {'sender': 'Human', 'content': 'In welcher Aufgabe habe ich die meisten Punkte bekommen und wie viele Punkte waren das?'},
-        {'sender': 'AI', 'content': 'Sie haben die meisten Punkte in der Aufgabe zum Themengebiet "ARIS ER-Modell" bekommen. Sie haben insgesamt 17,5 von 20 Punkten erreicht.'},
-        {'sender': 'Human', 'content': 'Was musste man in dieser Aufgabe tun?'},
-        {'sender': 'AI', 'content': 'In dieser Aufgabe musste man ein gegebenes ER-Modell in ein Tabellenschema überführen können und fachliche Fragen zu Tabellenschemata beantworten können.'}
-    ]
-    }
-    call_openai("Was habe ich dich als erstes gefragt?",conversation)
-    #main("was ist deine aufgabe?",['Musterloesung_Klausur.pdf','Klausur_Report.pdf'],conversation)
+    print("Hier gehts nicht weiter")
+    pass
